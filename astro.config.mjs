5307d7aaca545fbe0f349737b10c063753280385
asatani <you@example.com>
2025-12-01 09:22:41 +0900

    github

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
-// https://astro.build/config
+const isGhPages = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
+    site: isGhPages
+        ? 'https://yourname.github.io'
+        : 'http://localhost:4321',
+    base: isGhPages ? '/my-astro-site/' : '/', // ← 本番だけサブディレクトリ
     vite: {
-        plugins: [yaml()]
-    }
+        plugins: [yaml()],
+    },
 });
